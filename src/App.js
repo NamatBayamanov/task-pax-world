65f4c2071afd3abcc74fe968b1d36d1ae623f4b8
Namat Bayamanov <namataxvd15@gmail.com>
2022-07-05 00:08:40 +0600

    added anew

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 
-// import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-
-
 
 function App() {
   return (
     <div className="App">
-      <Layout/>
     </div>
   );
 }
